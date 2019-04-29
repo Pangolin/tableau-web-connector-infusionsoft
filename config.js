1d8e62f5397e04042243a5964140e572c16acb50
ZAFAR\Syed Zafar Naqvi <zanaqvi@gmail.com>
2019-04-29 18:24:46 +0500

    Updated - removed credentials

---
+++ config.js
@@ -0,0 +1,8 @@
+// The necessary configuration for your server
+// Contains credentials for your Foursquare application
+// And the new redirect path for the OAuth flow
+module.exports = {
+ 'HOSTPATH': 'http://localhost',
+ 'PORT': 3333,
+ 'REDIRECT_PATH': '/redirect'
+};
